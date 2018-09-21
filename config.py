f2a59079a51665e90fd793665a70add79af756aa
Soldier101 <aruso80@gmail.com>
2018-09-21 19:26:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 #token configuration
 # Legacy token system
-#BOTTOKEN = 'Yourtokenhere'
+#BOTTOKEN = 'Yourtokenhere' #uncomment and put your token here if you want it to be unsecure
 # new tokens (env var named BOTTOKEN need to contain the token for the bot)
-BOTTOKEN = (os.environ['BOTTOKEN'])
+BOTTOKEN = (os.environ['BOTTOKEN']) #comment out if you want to use the plain text token
+
